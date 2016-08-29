d5c5de38664597e2cb7329a0c0db666eb7b9c996
vinay <codehelper11@gmail.com>
2016-08-29 23:18:25 +0530

    updated controller to add pnr

--- src/main/java/com/droid/app/controller/PnrController.java
+++ src/main/java/com/droid/app/controller/PnrController.java
@@ -3,8 +3,11 @@ package com.droid.app.controller;
 import com.droid.app.entity.Greeting;
 import com.droid.app.entity.PnrStatus;
 import com.droid.app.service.PnrService;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
 
 /**
  * Created by vinay on 19/8/16.
@@ -19,10 +22,18 @@ public class PnrController {
         this.pnrService = pnrService;
     }
 
-    @RequestMapping("/hello")
+    @RequestMapping("/test")
     public PnrStatus test(){
 
         return pnrService.getPnrStatus("8750135595");
 
     }
+
+    @RequestMapping("/pnr/{pnr}")
+    public PnrStatus getPnrStatus(@PathVariable("pnr") String pnr){
+        return pnrService.getPnrStatus(pnr);
+
+    }
+
+
 }
