7e6ac10a90d7147353830240389c9512d3be9855
vinay <codehelper11@gmail.com>
2016-09-11 18:25:09 +0530

    tomcat 7 support

--- src/main/java/com/droid/app/Application.java
+++ src/main/java/com/droid/app/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+
 /**
  * Created by vinay on 19/8/16.
  */
